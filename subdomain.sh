fb7c7a7a750af61bb19128ae75399e5be823c330
Rajib prosad <108086539+mroddit@users.noreply.github.com>
2023-12-13 11:56:35 +0600

    subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
+#created by Mr.0ddit
+echo "#################################################"
+echo "#                                               #"
+echo "#                   Mr.0ddit                    #"
+echo "#                                               #"
+echo "#################################################"
+
+
 
 echo -n "Enter Your Domain:"
 read domain
 
 mkdir -p $domain $domain/aquatone $domain/wabackurls
 
-subfinder -d $domain |tee -a s1.txt
+subfinder -d $domain |tee -a $domain/s1.txt
 
-assetfinder --subs-only $domain |tee -a s2.txt
+assetfinder --subs-only $domain |tee -a $domain/s2.txt
 
-(curl -s https://crt.sh/\?q\=%25.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a s3.txt)
+(curl -s https://crt.sh/\?q\=%25.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a $domain/s3.txt)
 
-cat *.txt | grep -E *$domain | sort -u | tee -a s4.txt
+cat *.txt | grep -E *$domain | sort -u | tee -a $domain/s4.txt
 
-cat s4.txt | httpx | tee -a s5.txt
+cat $domain/s4.txt | httpx | tee -a $domain/s5.txt
 
-cat s5.txt | httprobe | tee -a s6.txt
+cat $domain/s5.txt | httprobe | tee -a $domain/s6.txt
 
-cat *.txt | grep -E *.$domain | sort -u | tee -a main.txt
+cat $domain/*.txt | grep -E *.$domain | sort -u | tee -a $domain/main.txt
 
-rm -rf s1.txt s2.txt s3.txt s4.txt s5.txt s6.txt
+rm -rf $domain/s1.txt $domain/s2.txt $domain/s3.txt $domain/s4.txt $domain/s5.txt $domain/s6.txt
 
-cat main.txt | aquatone -ports large | tee -a $domain/aquatone/0.txt
+cat $domain/main.txt | aquatone -ports large > aquatone/file.txt 
 
-cat main.txt | waybackurls > $domain/wabackurls/wayback.txt | egrep -v "\.woff|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.svg|\.css|\.ico|\.html" | sed 's/:80//g;s/443//g' | sort -u 
+cat $domain/main.txt | waybackurls > $domain/wabackurls/row.txt 
+cat $domain/wabackurls/wayback.txt | egrep -v "\.woff|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.svg|\.css|\.ico|\.html" | sed 's/:80//g;s/443//g' | sort -u >> wayback.txt
+
+
+#cat main.txt | waybackurls > wayback.txt | egrep -V "\.woff|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.svg|\.css|\.ico|\.html" | sed 's/:80//g;s/443//g' | sort -u
+
+
+#cat $domain/main.txt | aquatone -ports large > aquatone/0.txt | tee -a $domain/aquatone/0.txt
 
 
 
